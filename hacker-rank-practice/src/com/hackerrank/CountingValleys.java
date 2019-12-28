76e9d6719889f512cb8920c726a7c05611f9a8aa
Rafael Montero <rafaelmonterov@gmail.com>
2019-12-28 12:16:29 -0800

    matching variable name with hackerrank

--- hacker-rank-practice/src/com/hackerrank/CountingValleys.java
+++ hacker-rank-practice/src/com/hackerrank/CountingValleys.java
@@ -4,19 +4,19 @@ package com.hackerrank;
 
 public class CountingValleys {
 
-    public static int countingValleys(String path){
+    public static int countingValleys(String s){
 
         int result = 1;
         int valleys = 0;
 
-        for(int i = 0; i < path.length(); i++){
+        for(int i = 0; i < s.length(); i++){
 
-            if(path.charAt(i) == 'U')
+            if(s.charAt(i) == 'U')
                 result += 1;
             else
                 result -= 1;
 
-            if( i > 0 && path.charAt(i) == 'U' && result == 1)
+            if( i > 0 && s.charAt(i) == 'U' && result == 1)
                 valleys++;
         }
 
